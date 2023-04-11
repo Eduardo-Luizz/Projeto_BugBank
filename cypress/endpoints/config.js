06fa52c09c52b6b96aa5301a18575de565a86ceb
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-11 11:00:10 -0300

    Criação de objetos com valor padrão

---
+++ cypress/endpoints/config.js
@@ -0,0 +1,12 @@
+const urls = {
+    home: "https://bugbank.netlify.app/",
+    initial_page: "https://bugbank.netlify.app/home"
+}
+
+const fixed_messages = {
+    message_name: 'Olá João,',
+    message_welcome: 'bem vindo ao BugBank :)',
+    message_invalid_login: 'Usuário ou senha inválido. Tente novamente ou verifique suas informações!'
+}
+
+module.exports = { urls, fixed_messages }
