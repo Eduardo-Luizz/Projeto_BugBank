4ee2a3572a114a137885be3af3bbab2758493580
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-12 14:22:32 -0300

    Adição de mais parâmetros com valor fixos

--- cypress/endpoints/config.js
+++ cypress/endpoints/config.js
@@ -6,7 +6,8 @@ const urls = {
 const fixed_messages = {
     message_name: 'Olá João,',
     message_welcome: 'bem vindo ao BugBank :)',
-    message_invalid_login: 'Usuário ou senha inválido. Tente novamente ou verifique suas informações!'
+    message_invalid_login: 'Usuário ou senha inválido. Tente novamente ou verifique suas informações!',
+    input_warning_message: 'É campo obrigatório'
 }
 
 module.exports = { urls, fixed_messages }
