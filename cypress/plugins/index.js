97bf19634a1af45adb2df57c9470e3deb6b6486c
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-12 15:14:56 -0300

    Adicionando configuração para gerar report dos testes

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,5 +1,12 @@
 const cucumber = require('cypress-cucumber-preprocessor').default
 
+//
+const { merge } = require('mochawesome-merge');
+const { create } = require('mochawesome-report-generator');
+const path = require('path');
+const fs = require('fs-extra');
+//
+
 module.exports = (on, config) => {
   on('file:preprocessor', cucumber())
 }
@@ -26,3 +33,31 @@ const webpackOptions = {
 module.exports = (on) => {
   on('file:preprocessor', webpack(webpackOptions))
 }
+
+module.exports = (on, config) => {
+  on('after:run', (results) => {
+    const outputDir = path.join(process.cwd(), 'mochawesome-report');
+
+    // Create the report directory if it does not exist
+    if (!fs.existsSync(outputDir)) {
+      fs.mkdirSync(outputDir);
+    }
+
+    const reportFiles = fs.readdirSync(outputDir).filter((f) => f.endsWith('.json'));
+
+    merge({
+      files: reportFiles.map((file) => path.join(outputDir, file)),
+    }).then((report) =>
+      create({
+        reportDir: outputDir,
+        reportFilename: 'index.html',
+        reportTitle: 'Relatório de Testes',
+        reportPageTitle: 'Relatório de Testes',
+        reportJsonFilename: 'report.json',
+        charts: true,
+        overwrite: true,
+        timestamp: new Date().toISOString(),
+      }).then(() => console.log('Relatório de testes gerado com sucesso!'))
+    );
+  });
+};
