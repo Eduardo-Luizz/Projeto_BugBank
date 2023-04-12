418c2f05e945e229977734dacc0b3be1d9020aac
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-12 15:14:08 -0300

    Adicionando configuração para gerar report dos testes

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,10 @@ const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
 const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild");
+const { merge } = require('mochawesome-merge');
+const { create } = require('mochawesome-report-generator');
+const fs = require('fs-extra');
+const path = require('path');
 
 async function setupNodeEvents(on, config) {
 
@@ -22,6 +26,33 @@ module.exports = defineConfig({
     specPattern: "cypress/e2e/**/*.feature",
     setupNodeEvents,
   },
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/reports/mochawesome',
+    reportFilename: 'index.html',
+    overwrite: false,
+    html: true,
+    json: true
+  },
+  after(runResults, args) {
+    return merge(runResults)
+      .then((report) => {
+        const reportDir = path.join(process.cwd(), 'cypress/reports/mochawesome');
+        fs.mkdirpSync(reportDir);
+        const reportFilename = path.join(reportDir, 'report.json');
+        fs.writeFileSync(reportFilename, JSON.stringify(report));
+        return create({
+          reportDir: reportDir,
+          reportFilename: 'index.html',
+          reportTitle: 'Relatório de Testes',
+          reportPageTitle: 'Relatório de Testes',
+          reportJsonFilename: 'report.json',
+          charts: true,
+          overwrite: false,
+          timestamp: new Date().toISOString(),
+          inline: true,
+          assetsFolder: 'assets',
+        });
+      });
+  }
 });
-
-//specPattern: "**/*.feature",
