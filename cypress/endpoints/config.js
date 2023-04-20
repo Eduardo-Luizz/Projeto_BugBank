50daa00043a1f03ead5e6da6ce7789f1b1f2ef9f
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-20 13:15:08 -0300

    Adicionando mais mensagens fixas referentes a tela inicial

--- cypress/endpoints/config.js
+++ cypress/endpoints/config.js
@@ -7,7 +7,11 @@ const fixed_messages = {
     message_name: 'Olá João,',
     message_welcome: 'bem vindo ao BugBank :)',
     message_invalid_login: 'Usuário ou senha inválido. Tente novamente ou verifique suas informações!',
-    input_warning_message: 'É campo obrigatório'
+    input_warning_message: 'É campo obrigatório',
+    label_option_transfer: 'TRANSFERÊNCIA',
+    label_option_payment: 'PAGAMENTOS',
+    label_option_extract: 'EXTRATO',
+    label_option_withdraw: 'SAQUE',
 }
 
 module.exports = { urls, fixed_messages }
