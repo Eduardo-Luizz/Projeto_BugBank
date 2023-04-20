37ed52be1c069ee5bb4f0ac97106e9000778a70c
Eduardo Luiz Zanotto <91149760+Eduardo-Luizz@users.noreply.github.com>
2023-04-20 13:16:09 -0300

    Adicionando elementos que são passíveis de iteração na tela inicial

---
+++ cypress/pages/home_page/elements.js
@@ -0,0 +1,11 @@
+export const ELEMENTS = {
+    LIST: {
+        LABEL_TRANSFER: `//*[@id="__next"]/div/div[3]/div[2]/div[1]/p`,
+        LABEL_PAYMENT: `//*[@id="__next"]/div/div[3]/div[2]/div[2]/p`,
+        LABEL_EXTRACT: `//*[@id="__next"]/div/div[3]/div[2]/div[3]/p`,
+        LABEL_WITHDRAW: `//*[@id="__next"]/div/div[3]/div[2]/div[4]/p`,
+        LABEL_BALANCE_HOME_PAGE: `//*[@id="textBalance"]`,
+        BUTTON_EXIT: `//*[@id="__next"]/div/div[1]/div`,
+        BUTTON_LOGO: `//*[@id="__next"]/div/div[1]/a/span/img`
+    }
+}
